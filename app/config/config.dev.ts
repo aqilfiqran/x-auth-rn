4bbbcab90e1b79dd5dc635c67194027869dae405
aqilfiqran <aqilfiqran19@gmail.com>
2024-10-04 15:09:57 +0700

    chore: config and devtools

---
+++ app/config/config.dev.ts
@@ -0,0 +1,11 @@
+import ENV from "react-native-config"
+/**
+ * These are configuration settings for the dev environment.
+ *
+ * Do not include API secrets in this file or anywhere in your JS.
+ *
+ * https://reactnative.dev/docs/security#storing-sensitive-info
+ */
+export default {
+  ...ENV,
+}
---
+++ app/i18n/id.ts
@@ -0,0 +1,6 @@
+const id = {
+  common: "Umum",
+}
+
+export default id
+export type Translations = typeof id
